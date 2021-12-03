ead945a83b0da195e26f3cd704aa42e1c099a7a4
Elan Goens <elan.goens@student.hogent.be>
2021-12-03 13:20:17 +0100

    First commit

---
+++ BookshelfIOS/BookshelfIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookshelfIOSApp.swift
+//  BookshelfIOS
+//
+//  Created by Elan Goens on 03/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct BookshelfIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
